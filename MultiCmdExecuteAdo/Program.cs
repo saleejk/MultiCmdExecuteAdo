5a42c6ba1f3db2bd069986ee421eb02b0659ad25
saleejk <saleejckd4ever@gmail.com>
2024-05-11 14:48:05 +0530

    Add project files.

---
+++ MultiCmdExecuteAdo/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string connectionString= "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
+        try
+        {
+            using(SqlConnection connection =new SqlConnection(connectionString))
+            {
+                if (connection.State == ConnectionState.Closed) 
+                    connection.Open();
+                using (SqlCommand command = new SqlCommand("select * from student; select * from teacher",connection)){
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Console.WriteLine(reader[1] + " " + reader[2]);
+                        }
+                        while (reader.NextResult())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(reader[1] + " " + reader[2]);
+                            }
+                        }
+                    }
+                }
+                {
+
+                }
+
+            }
+        }catch (SqlException ex)
+        {
+            Console.WriteLine("Sql exception is occured",ex);
+        }catch (Exception ex)
+        {
+            Console.WriteLine("Wxception is occured",ex);
+
+        }
+    }
+}
