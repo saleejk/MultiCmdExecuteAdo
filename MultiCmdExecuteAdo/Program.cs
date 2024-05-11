fc819a15f813fd73c4fd133ebb3946185486ceac
saleejk <saleejckd4ever@gmail.com>
2024-05-11 15:58:05 +0530

    Added new function MultiCommandExecuteWithParameter

--- MultiCmdExecuteAdo/Program.cs
+++ MultiCmdExecuteAdo/Program.cs
@@ -5,18 +5,63 @@ class Program
 {
     public static void Main(string[] args)
     {
-
+        Program program=new Program();
+        program.MultiCommandExecuteWithParameter("student", "teacher");
+     
+    }
+    public void MultiCommandExecuteWithParameter(string table1,string table2)
+    {
         string connectionString= "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
-        try
+        using(SqlConnection connection=new SqlConnection(connectionString))
         {
-            using(SqlConnection connection =new SqlConnection(connectionString))
-            {
-                if (connection.State == ConnectionState.Closed) 
-                    connection.Open();
-                using (SqlCommand command = new SqlCommand("select * from student; select * from teacher",connection)){
+            if (connection.State == ConnectionState.Closed)
+            
+                connection.Open();
+            SqlCommand command = new SqlCommand($"select * from {table1};select * from {table2}", connection);
+                
+                    //command.Parameters.AddWithValue("table1", table1);
+                    //command.Parameters.AddWithValue("table2", table2);
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        while(reader.Read())
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader[1]);
+                        }
+                        while (reader.NextResult())
+                        {
+
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(reader[1] + " " + reader[2]);
+                            }
+
+                        }
+                    }
+                
+            
+        }
+    }
+
+
+
+
+
+
+
+    public void MultiCommandExecute()
+    {
+        string connectionString = "data source= SALEEJK; database= master;integrated security=SSPI;TrustServerCertificate=True";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                using (SqlCommand command = new SqlCommand("select * from student; select * from teacher", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
                         {
                             Console.WriteLine(reader[1] + " " + reader[2]);
                         }
@@ -34,12 +79,14 @@ class Program
                 }
 
             }
-        }catch (SqlException ex)
+        }
+        catch (SqlException ex)
         {
-            Console.WriteLine("Sql exception is occured",ex);
-        }catch (Exception ex)
+            Console.WriteLine("Sql exception is occured", ex);
+        }
+        catch (Exception ex)
         {
-            Console.WriteLine("Wxception is occured",ex);
+            Console.WriteLine("Wxception is occured", ex);
 
         }
     }
